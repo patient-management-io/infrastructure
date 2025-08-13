a177a2348a5cdb94e9c21e630ab39701daf76091
Connor Burge <connor.d.burge@gmail.com>
2025-08-13 12:39:00 -0400

    added createDatabase to stack

--- src/main/java/com/pm/stack/LocalStack.java
+++ src/main/java/com/pm/stack/LocalStack.java
@@ -1,7 +1,11 @@
 package com.pm.stack;
 
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.InstanceClass;
+import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.rds.*;
 
 public class LocalStack extends Stack {
 
@@ -10,12 +14,32 @@ public class LocalStack extends Stack {
     public LocalStack(final App scope, final String id, final StackProps props) {
         super(scope, id, props);
         this.vpc = createVpc();
+        DatabaseInstance authServiceDB = createDatabase("AuthServiceDB", "auth-service-db");
+        DatabaseInstance patientServiceDB = createDatabase("PatientServiceDB", "patient-service-db");
     }
 
     private Vpc createVpc() {
-        return Vpc.Builder.create(this, "PatientManagementVPC")
+        return Vpc.Builder
+                .create(this, "PatientManagementVPC")
                 .vpcName("PatientManagementVPC")
                 .maxAzs(2)
+                .build();
+    }
+
+    private DatabaseInstance createDatabase(String id, String dbName) {
+        return DatabaseInstance.Builder
+                .create(this, id)
+                .engine(DatabaseInstanceEngine.postgres(
+                        PostgresInstanceEngineProps.builder()
+                                .version(PostgresEngineVersion.VER_17_2)
+                                .build()
+                        ))
+                .vpc(vpc)
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
+                .allocatedStorage(20)
+                .credentials(Credentials.fromGeneratedSecret("admin_user"))
+                .databaseName(dbName)
+                .removalPolicy(RemovalPolicy.DESTROY)
                 .build();
     }
 
